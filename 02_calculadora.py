229cd3cbc70f0b2319cc8e9a5c2b92700fc85061
Mariana Stamatakis <mst.nanasta@gmail.com>
2025-11-11 11:11:32 -0300

    Feat: Aulas de primeiros comandos

---
+++ 02_calculadora.py
@@ -0,0 +1,24 @@
+#%%
+#Soma
+print("10-5=", 10-5)
+
+#Subtração
+
+print("10-5=", 10-5)
+
+#%%
+#Multiplicação e divisão
+print("10x5=", 10*5)
+print("10/5=", 10/5)
+
+#%%
+# divisão com inteira parte 
+print("10//3=",10//3)
+# %%
+# Resto da divisão
+print("10%4=",10%4) 
+
+# %%
+# Potenciação
+print("3**4=",3**4) 
+# %%
