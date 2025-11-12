d5fc0c7753f8e00b6152c87dc12b7745cc10d701
Mariana Stamatakis <mst.nanasta@gmail.com>
2025-11-12 14:54:17 -0300

    feat: Operadores lógicos, If e Else.

---
+++ dia_02/07_else.py
@@ -0,0 +1,13 @@
+# %%
+
+idade = 20
+
+if idade >= 18:
+    print("Você pode beber cerveja")
+    print("Beba com moderação!") 
+
+else:
+    print("Você não pode beber cerveja") 
+    print("Vá pra casa beber leite!") 
+    
+# %%
